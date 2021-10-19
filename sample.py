6f48a49dc7289bccb850c098fe8a32f9f77bbf08
ngepippi <ngepippi.0517@gmail.com>
2021-10-19 16:36:05 +0900

    四則演算の関数を追加

--- sample.py
+++ sample.py
@@ -1,8 +1,20 @@
 def add_(a,b):
     return a+b
 
+def sub_(a,b):
+    return a-b
+
+def mul_(a,b):
+    return a*b
+
+def div_(a,b):
+    return a/b
+
 if __name__ == "__main__":
     a = 10 
     b = 20
 
     print(add_(a,b))
+    print(sub_(a,b))
+    print(mul_(a,b))
+    print(div_(a,b))
